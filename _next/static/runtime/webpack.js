8da0d4f59bb995cc63ec11875362ade13433ebb7
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-05-04 23:32:51 +0400

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "690d7913299900d33b79";
+/******/ 	var hotCurrentHash = "38844ef786d236d3cce1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
