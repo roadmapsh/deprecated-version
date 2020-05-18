9db9f0e253a85f4005230ca547ea550337bb30e9
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-05-19 01:22:44 +0400

    Updates

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1589405731002");
+            injectCss(link, link.href.split("?")[0] + "?unix=1589836412048");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1589405731644");
+            injectCss(link, link.href.split("?")[0] + "?unix=1589836412773");
           });
       }
     }
--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "68fcffdaa3f5b67dc021";
+/******/ 	var hotCurrentHash = "eea883e862abc28bceee";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
