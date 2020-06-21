349ad1faa5c2cdf24c9d93f0815f25108b4aa8c7
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-06-21 20:22:13 +0000

    Updates

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/[roadmap]":[a,b],"/guides":[a,b],"/guides/[guide]":[a,b],"/roadmaps":[a,b]}}("static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
