5ed54a35ebe9c1b32c4d17acff11b60479fe7a93
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-08-27 16:27:42 +0400

    Add minification for javascript

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const { minify } = require('terser');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
@@ -6,10 +7,25 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/styles/main.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
 
+  // Rebuild tailwind before reloading
   eleventyConfig.on('eleventy.after', async () => {
     console.log('Building Tailwind…');
     console.log(execSync('npm run build:tailwind').toString());
   });
+
+  // Minify JavaScript
+  eleventyConfig.addNunjucksAsyncFilter(
+    'jsmin',
+    async function (code, callback) {
+      try {
+        const minified = await minify(code);
+        callback(null, minified.code);
+      } catch (err) {
+        console.error('Terser error: ', err);
+        callback(null, code);
+      }
+    }
+  );
 
   return {
     htmlTemplateEngine: 'njk',
