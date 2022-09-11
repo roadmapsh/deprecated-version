a982fef77b01a9d4222e1c4351b8e62c693a9616
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-09-11 23:22:09 +0000

    Updates

--- assets/renderer.js
+++ assets/renderer.js
@@ -1,3 +1,10 @@
+function createElementFromHTML(htmlString) {
+  const div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  return div.firstChild;
+}
+
 function renderRoadmap(jsonUrl) {
   if (!jsonUrl) {
     console.error('jsonUrl not defined in frontmatter');
@@ -16,5 +23,22 @@ function renderRoadmap(jsonUrl) {
     })
     .then(function (json) {
       return renderer.wireframeJSONToSVG(json);
+    });
+}
+
+function fetchTopic(roadmapId, groupId) {
+  const topicPartial = groupId.replace(/^\d+-/, '').replaceAll(/:/g, '/');
+  const fullUrl = `/${roadmapId}/${topicPartial}/`;
+
+  return fetch(fullUrl)
+    .then((res) => {
+      return res.text();
+    })
+    .then((topicHtml) => {
+      // It's full HTML with page body, head etc
+      // We only need the inner HTML of the #topic-content
+      const node = new DOMParser().parseFromString(topicHtml, 'text/html');
+
+      return node.querySelector('#topic-content');
     });
 }
