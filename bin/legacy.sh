99d1c972e5dd0ad6c90b73fd38ebfd73c209fb4b
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-11-10 15:40:05 +0400

    Fix iframe in the videos

--- bin/legacy.sh
+++ bin/legacy.sh
@@ -7,7 +7,7 @@ if [ ! -d "./developer-roadmap" ]; then
   git clone --depth 1 -b master git@github.com:kamranahmedse/developer-roadmap.git
 fi
 
-echo "Removing old directories"
+echo -e "\nRemoving old directories"
 rm -rf ../src/videos
 rm -rf ../src/guides
 rm -rf ../src/roadmaps
@@ -15,6 +15,14 @@ rm -rf ../src/roadmaps
 rm -rf ../src/assets/jsons
 rm -rf ../src/assets/pdfs
 
+echo -e "\n=== Migrating Roadmaps ==="
 node roadmap-migrator.js
+
+echo -e "\n=== Migrating Content ==="
 node content-migrator.js
+
+echo -e "\n=== Migrating Guides ==="
 node guide-migrator.js
+
+echo -e "\n=== Migrating Videos ==="
+node video-migrator.js
---
+++ src/videos/promises-in-javascript.md
@@ -0,0 +1,16 @@
+---
+layout: "layouts/video.njk"
+title: "All about Promises in JavaScript"
+description: "Learn how to write asynchronous code in JavaScript using promises."
+duration: "8 minutes"
+isNew: false
+date: 2020-07-20
+sitemap:
+  priority: 0.7
+  changefreq: "weekly"
+tags:
+  - "video"
+  - "video-sitemap"
+---
+
+<iframe class="w-full aspect-video mb-5" src="https://www.youtube.com/embed/BvrkobaCVVE" title="All about Promises in JavaScript" />
