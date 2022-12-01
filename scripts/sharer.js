17ffe56c2b0ee433e7de3143544b537d802de9a3
Kamran Ahmed <kamranahmed.se@gmail.com>
2022-12-01 03:04:42 +0000

    Updates

---
+++ scripts/sharer.js
@@ -0,0 +1,2 @@
+(()=>{var s=class{constructor(){this.init=this.init.bind(this),this.onScroll=this.onScroll.bind(this),this.shareIconsId="page-share-icons"}get shareIconsEl(){return document.getElementById(this.shareIconsId)}onScroll(){if(window.scrollY<100||window.innerWidth<1050)return this.shareIconsEl.classList.add("hidden"),null;this.shareIconsEl.classList.remove("hidden")}init(){window.addEventListener("scroll",this.onScroll,{passive:!0})}};})();
+//# sourceMappingURL=sharer.js.map
